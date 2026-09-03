4fdd1b1c74a21c1ad628dc389209aee9f55dec10
Marc-Antoine Ruel <maruel@gmail.com>
2026-09-03 09:25:10 -0400

    Keep dictation listening continuously

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 // Bump on every change to a cached asset, else visitors keep the old version.
 // See AGENTS.md.
-const CACHE_NAME = 'textarea-2026-09-02-9'
+const CACHE_NAME = 'textarea-2026-09-03'
 const ASSETS = [
   '/',
 ]
